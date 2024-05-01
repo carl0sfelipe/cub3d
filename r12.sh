8654889cf57988821111997183f044aee6aa31b1
carl0sfelipe <37373901+carl0sfelipe@users.noreply.github.com>
2024-05-01 17:00:47 -0300

    Delete r12.sh

--- r12.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Traverse the directory and subdirectories for .c files
-find . -type f -name "*.c" -print0 | while IFS= read -r -d '' file; do
-    # Remove the first 12 lines from each .c file found
-    tail -n +13 "$file" > "$file.tmp" && mv "$file.tmp" "$file"
-done
-
